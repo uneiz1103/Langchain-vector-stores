43a2afad2457a2eb8506193d271e607b75b07dcf
uneiz1103 <uneizshaikh1103@gmail.com>
2025-08-06 12:16:36 +0530

    feat: add document update support in Chroma vector store

--- chroma.py
+++ chroma.py
@@ -58,3 +58,13 @@ vector_store.similarity_search_with_score(
     filter={'team': 'chennai super kings'}
 )
 
+# update documents
+updated_doc1 = Document(
+    page_content="Virat Kohli, the former captain of Royal Challengers Bangalore (RCB), is renowned for his aggressive leadership and consistent batting performances. He holds the record for the most runs in IPL history, including multiple centuries in a single season. Despite RCB not winning an IPL title under his captaincy, Kohli's passion and fitness set a benchmark for the league. His ability to chase targets and anchor innings has made him one of the most dependable players in T20 cricket.",
+    metadata={"team": "Royal Challengers Bangalore"}
+)
+
+vector_store.update_document(document_id='09a39dc6-3ba6-4ea7-927e-fdda591da5e4', document=updated_doc1)
+
+
+
